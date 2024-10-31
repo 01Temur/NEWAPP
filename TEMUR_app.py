6d4a267e34878768763033f1e7d07d9643fb70aa
01Temur <tjurabekov@iuj.ac.jp>
2024-10-31 14:14:33 +0900

    Update TEMUR_app.py

--- TEMUR_app.py
+++ TEMUR_app.py
@@ -1,6 +1,7 @@
-import ipywidgets as widgets
-from IPython.display import display
-from IPython.display import Image
-image_widget = Image(filename='IUJ.jpg')
-text_widget = widgets.HTML(value="<h1>IUJ. WELCOME TO Investment and Machine Learning COURSE</h1>")
-app = widgets.VBox([image_widget, text_widget])
+import streamlit as st
+
+# Title and description
+st.title("IUJ. WELCOME TO Investment and Machine Learning COURSE")
+
+# Display image
+st.image("IUJ.jpg", use_column_width=True)
